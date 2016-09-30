c5a4a6831c9c37739a9307241717760324092dea
billbunkum <william.richard.hurst@gmail.com>
2016-09-30 14:15:12 -0400

    make global_barks_list username links point to author's barks_list

--- barks/urls.py
+++ barks/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^users?/$', views.global_barks_list, name="global_barks_list"),
+    url(r'^users/$', views.global_barks_list, name="global_barks_list"),
     url(r'^add/$', views.add_bark, name="add_bark"),
     url(r'^(?P<id>\d+)/$', views.barks_list, name="barks_list"),
     url(r'^$', views.barks_list, name="barks_list"),
